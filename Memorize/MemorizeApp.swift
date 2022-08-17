022713de272da03611613cc2d3de13bbdc639302
Danny Wade <danny.lee.wade@gmail.com>
2022-08-17 20:41:35 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Danny Wade on 17/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
